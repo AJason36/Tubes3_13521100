b5a1d61f79fee40d29f3732c310ab9a7f3d14f86
Michaelu670 <michaelu670@gmail.com>
2023-05-03 13:37:38 +0700

    feat: add date

---
+++ gpt-1/src/lib/date.ts
@@ -0,0 +1,23 @@
+/**
+ * 
+ * @param date string, format = 'DD-MM-YYYY or DD/MM/YYYY'
+ */
+const getDayFromDate = function(date: string): string {
+    let arr = date.match(/(\d+)[-\/](\d+)[-\/](\d+)/)
+
+    let days = [
+        "Minggu",
+        "Senin",
+        "Selasa",
+        "Rabu",
+        "Kamis",
+        "Jumat",
+        "Sabtu"
+    ]
+    if (arr && arr.length >= 4 && arr[1] && arr[2] && arr[3])
+        return days[new Date(Number.parseInt(arr[3]), Number.parseInt(arr[2]) - 1, Number.parseInt(arr[1])).getDay()] ?? 'undefined'
+    throw Error('Wrong date')
+}
+
+// console.log(getDayFromDate('03-05-2023'))
+// console.log(getDayFromDate('03/05/2023'))
