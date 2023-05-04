ea0c69f2b0cb77e8ca88b625e07e7439f957fc3e
Alexander Jason <alexanderjason526@gmail.com>
2023-05-04 12:23:06 +0700

    feat: add new chat button

---
+++ gpt-1/components/button/NewChat.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+
+const NewChat = () => {
+  return (
+    <div className="relative w-[284.59px] h-[45.14px] shrink-0">
+      <div className="absolute top-[0px] left-[0px] w-[284.59px] h-[45.14px]">
+        <button
+          className="absolute top-[0px] left-[0px] rounded-[10px] w-[17.813rem] h-11 hover:bg-[#329e5a] bg-[#45DF7D]"
+          type="button"
+        >
+          <div className="absolute top-[10px] left-[100px] font-semibold inline-block h-[16.68px]">
+            + New Chat
+          </div>
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewChat;
