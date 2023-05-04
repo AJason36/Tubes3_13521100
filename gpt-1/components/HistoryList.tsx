06281e020e474ce85fd953babc2c6a092c3e8584
Alexander Jason <alexanderjason526@gmail.com>
2023-05-04 23:46:59 +0700

    style: change size

--- gpt-1/components/HistoryList.tsx
+++ gpt-1/components/HistoryList.tsx
@@ -6,17 +6,19 @@ import { ChatSession } from "types";
 
 const HistoryList = ({sessions, setSessions}: { sessions: ChatSession[], setSessions: React.Dispatch<React.SetStateAction<ChatSession[]>> }) => {
   return (  
-      <div className="absolute top-[5rem] left-[1rem] flex flex-col items-start justify-start gap-[1.25rem] ">
+    <>
+      <div className="relative top-[7%] left-[1rem] flex flex-col gap-[1.25rem] w-[90%] ">
       <NewChat setSessions={setSessions} />
       <div className="flex flex-col items-start justify-start gap-[10px] text-[1.5rem]">
         <div className="relative font-medium">Chat List</div>
-        <div className="flex flex-col items-start max-h-[20rem] justify-start gap-[2rem] text-[0.75rem] text-lightgray-200 overflow-y-auto space-y-1 scrollbar-hide">
+        <div className="flex flex-col items-start w-[100%] max-h-[55%] justify-start gap-[2rem] text-[0.75rem] text-lightgray-200 overflow-y-auto space-y-1 scrollbar-hide">
           {sessions.map((session, index) => (
             <ChatHistory key={index} id={session.id} title={session.name} setSessions={setSessions} />
           ))}
         </div>
       </div>
       </div>
+      </>
   );
 };
 
