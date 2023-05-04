7c350861db75ba31a1bf9a4f70ea162e7de38482
Alexander Jason <alexanderjason526@gmail.com>
2023-05-04 15:31:24 +0700

    chore: change button type

--- gpt-1/components/button/Send.tsx
+++ gpt-1/components/button/Send.tsx
@@ -3,9 +3,9 @@ import Image from "next/image";
 
 const Send = () => {
     return (  
-        <div className="absolute top-[630px] left-[1140px] w-[60px] h-[60px] ">
+        <div className="absolute top-3 left-[68rem] w-[60px] h-[60px] ">
           <button className="absolute rounded-[10px] top-[0px] left-[0px] w-[40px] h-[40px] hover:bg-gray-600"
-                  type="button">
+                  type="submit">
                 <Image src="/assets/enter-button.svg" alt="enter button" width={40} height={40} />
               </button>
         </div>
