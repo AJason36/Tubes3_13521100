3236c7eb4c53ab1c3e8a975b94fe22e717cd198b
Alexander Jason <alexanderjason526@gmail.com>
2023-05-04 12:21:47 +0700

    feat: chat bubble

---
+++ gpt-1/components/ChatBubble.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+
+const ChatBubble = (message:any) => {
+    const isChatGPT = message.name == "GPT-1";
+    var avatar;
+    if (isChatGPT) {
+        avatar = "/assets/gpt-logo.svg";
+    } else {
+        avatar = "/assets/user-circle.svg";
+    }
+    return (  
+        <div className={`py-5 text-white ${!isChatGPT && "bg-[#24284F]"}`}>
+            <div className="flex space-x-5 px-10 max-w-xl mx-auto">
+                <img src={avatar}
+                    alt="" className="h-8 w-8 rounded-sm" />
+                <p className="pt-1 text-md font-medium">{message.text}</p>
+            </div>
+        </div>
+    );
+}
+ 
+export default ChatBubble;
