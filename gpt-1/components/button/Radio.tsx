f2b65daf2ef97ddebd62870b29855ee82efd7ac8
Alexander Jason <alexanderjason526@gmail.com>
2023-05-05 12:42:38 +0700

    fix: fix switch radio button

--- gpt-1/components/button/Radio.tsx
+++ gpt-1/components/button/Radio.tsx
@@ -5,36 +5,44 @@ const Radio = ({ setMode }: { setMode: React.Dispatch<React.SetStateAction<strin
 
     const handleRadioChange = (event: React.ChangeEvent<HTMLInputElement>) => {
         setSelectedOption(event.target.value);
-        setMode(selectedOption);
+        console.log(event.target.id);
+        if (event.target.id === "KMP-radio" && event.target.checked) {
+            setMode("KMP");
+            console.log("KMP");
+        } else if(event.target.id === "bm-radio" && event.target.checked){
+            setMode("BM");
+            console.log("BM");
+        } else {
+            setMode("");
+            console.log("none");
+        }
     };
   
     return (
       <div className="absolute top-[80%] left-[18px] w-[18%] h-[10%]">
         <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
           <input
-            id="bordered-radio-1"
+            id="KMP-radio"
             type="radio"
             value="KMP"
             name="bordered-radio"
             className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-blue-500 dark:focus:ring-blue-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            checked={selectedOption === "KMP"}
             onChange={handleRadioChange}
           />
-          <label htmlFor="bordered-radio-1" className="w-full py-4 ml-2 text-sm font-medium text-white dark:text-gray-300">
+          <label htmlFor="KMP-radio" className="w-full py-4 ml-2 text-sm font-medium text-white dark:text-gray-300">
             KMP
           </label>
         </div>
         <div className="flex items-center pl-4 border border-gray-200 rounded dark:border-gray-700">
           <input
-            id="bordered-radio-2"
+            id="bm-radio"
             type="radio"
             value="BM"
             name="bordered-radio"
             className="w-4 h-4 text-gray-600 bg-gray-100 border-gray-300 focus:ring-gray-600 dark:focus:ring-gray-600 dark:ring-offset-gray-800 focus:ring-2 dark:bg-gray-700 dark:border-gray-600"
-            checked={selectedOption === "BM"}
             onChange={handleRadioChange}
           />
-          <label htmlFor="bordered-radio-2" className="w-full py-4 ml-2 text-sm font-medium text-white dark:text-gray-300">
+          <label htmlFor="bm-radio" className="w-full py-4 ml-2 text-sm font-medium text-white dark:text-gray-300">
             BM
           </label>
         </div>
